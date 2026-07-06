e8c80a526138eeedfbde8de3a39dfb648f6e3bdf
Eman Alnajjar <emanalnajjar241@gmail.com>
2026-07-07 00:09:28 +0300

    add cart to DI & app route

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -8,6 +8,15 @@ import '../../features-provider/driver/domain/repositories/order_repository.dart
 import '../../features-provider/driver/domain/repositories/trip_repository.dart';
 import '../../features-provider/history/data/history_local_repository.dart';
 import '../../features-provider/history/domain/repositories/history_repository.dart';
+import '../../features_user/cart/data/data_sources/cart_local_data.dart';
+import '../../features_user/cart/data/repositories_impl/cart_repoditory_impl.dart';
+import '../../features_user/cart/domain/usecases/add_product.dart';
+import '../../features_user/cart/domain/usecases/clear_cart.dart';
+import '../../features_user/cart/domain/usecases/decrease_quantity.dart';
+import '../../features_user/cart/domain/usecases/delete_product.dart';
+import '../../features_user/cart/domain/usecases/get_cart.dart';
+import '../../features_user/cart/domain/usecases/increase_quantity.dart';
+import '../../features_user/cart/presentation/manager/cart_cubit.dart';
 
 class Locator {
   Locator({required SharedPreferences prefs})
@@ -17,6 +26,49 @@ class Locator {
   final OrderRepository orders = OrderSimulationRepository();
   final TripRepository trips = TripSimulationRepository();
   final HistoryRepository history;
+
+  ////////////////////cart
+  final CartLocalDataSource cartLocalDataSource =
+  CartLocalDataSource();
+
+
+  late final CartRepositoryImpl cartRepository =
+  CartRepositoryImpl(cartLocalDataSource);
+
+
+  late final AddProductUseCase addProductUseCase =
+  AddProductUseCase(cartRepository);
+
+
+  late final DeleteProductUseCase deleteProductUseCase =
+  DeleteProductUseCase(cartRepository);
+
+
+  late final GetCartUseCase getCartUseCase =
+  GetCartUseCase(cartRepository);
+
+
+  late final IncreaseQuantityUseCase increaseQuantityUseCase =
+  IncreaseQuantityUseCase(cartRepository);
+
+
+  late final DecreaseQuantityUseCase decreaseQuantityUseCase =
+  DecreaseQuantityUseCase(cartRepository);
+
+
+  late final ClearCartUseCase clearCartUseCase =
+  ClearCartUseCase(cartRepository);
+
+
+  late final CartCubit cartCubit = CartCubit(
+    addProductUseCase: addProductUseCase,
+    deleteProductUseCase: deleteProductUseCase,
+    getCartUseCase: getCartUseCase,
+    increaseQuantityUseCase: increaseQuantityUseCase,
+    decreaseQuantityUseCase: decreaseQuantityUseCase,
+    clearCartUseCase: clearCartUseCase,
+  )..loadCart();
+
 }
 
 late final Locator getIt;
@@ -24,4 +76,8 @@ late final Locator getIt;
 Future<void> initServiceLocator() async {
   final prefs = await SharedPreferences.getInstance();
   getIt = Locator(prefs: prefs);
+
+
 }
+
+
--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tracking_provider/features_user/cart/presentation/pages/cart.dart';
 
 import '../../features-provider/driver/presentation/screens/main/driver_main_screen.dart';
 import '../../features-provider/driver/presentation/screens/trip/trip_screen.dart';
 import '../../features-provider/history/presentation/screens/history_screen.dart';
 import '../../features-provider/splash/presentation/screens/splash_screen.dart';
+import '../di/service_locator.dart';
 
 
 class AppRouter {
@@ -19,8 +21,13 @@ class AppRouter {
       case HistoryScreen.route:
         return MaterialPageRoute(builder: (_) => const HistoryScreen());
         ///////////////// user Pages
-        case Cart.route:
-        return MaterialPageRoute(builder: (_) => const Cart());
+      case Cart.route:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider.value(
+            value: getIt.cartCubit,
+            child: const Cart(),
+          ),
+        );
         default:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
     }
