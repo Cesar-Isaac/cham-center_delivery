73152b2d50f2b247bde321478282561df6151f03
Eman Alnajjar <emanalnajjar241@gmail.com>
2026-07-13 02:39:52 +0300

    create cham_center location

---
+++ lib/core/constants/location_constants.dart
@@ -0,0 +1,9 @@
+class LocationConstants {
+  LocationConstants._();
+
+  static const String mallName = 'مول الشام سنتر';
+
+  // استبدليهما بالإحداثيات الحقيقية للمول
+  static const double mallLatitude = 0;
+  static const double mallLongitude = 0;
+}
